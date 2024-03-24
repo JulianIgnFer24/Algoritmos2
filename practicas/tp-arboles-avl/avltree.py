16912550d5afded52ac487abbd8a32e90af542e4
JulianIgnFer24 <f.julian2617@gmail.com>
2024-03-24 20:31:33 -0300

    arregle los rotate, hice el delete y el insert
    los rotate no andaban bien en los casos donde el nodo que pasabamos para rotar no era la raíz, era un tema de que no actualizaba bien los parents

--- practicas/tp-arboles-avl/avltree.py
+++ practicas/tp-arboles-avl/avltree.py
@@ -1,7 +1,6 @@
 class AVLTree:
   root = None
 
-
 class AVLNode:
   parent = None
   leftnode = None
@@ -13,31 +12,46 @@ class AVLNode:
 #rota a la izquierda
 def rotateLeft(Tree, avlnode):
   newRoot = avlnode.rightnode
+  avlnode.rightnode = newRoot.leftnode
 
-  if newRoot.leftnode == None:
-    if avlnode != Tree.root:
-      avlnode.parent = newRoot
-    else:
+
+  if newRoot.leftnode != None:
+      newRoot.leftnode.parent = avlnode
+  newRoot.parent = avlnode.parent
+
+
+  if avlnode.parent == None:
       Tree.root = newRoot
-    avlnode.rightnode = None
-    newRoot.leftnode = avlnode
+  else:
+      if avlnode.parent.leftnode == avlnode:
+          avlnode.parent.leftnode = newRoot
+      else:
+          avlnode.parent.rightnode = newRoot
+  newRoot.leftnode = avlnode
+  avlnode.parent = newRoot
 
-  return (newRoot)
+  return(newRoot)
 
 #rota a la derecha
 def rotateRight(Tree, avlnode):
   newRoot = avlnode.leftnode
+  avlnode.leftnode = newRoot.rightnode
 
-  if newRoot.rightnode == None:
-    if avlnode != Tree.root:
-      avlnode.parent = newRoot
-    else:
+  if newRoot.rightnode != None:
+      newRoot.rightnode.parent = avlnode
+  newRoot.parent = avlnode.parent
+
+
+  if avlnode.parent == None:
       Tree.root = newRoot
-    avlnode.leftnode = None
-    newRoot.rightnode = avlnode
-
-  return (newRoot)
-
+  else:
+      if avlnode.parent.rightnode == avlnode:
+          avlnode.parent.rightnode = newRoot
+      else:
+          avlnode.parent.leftnode = newRoot
+  newRoot.rightnode = avlnode
+  avlnode.parent = newRoot
+  return(newRoot)
 #calcula la altura desde un nodo dado
 def height(node):
   if node is None:
@@ -46,7 +60,6 @@ def height(node):
     height_left = height(node.leftnode)
     height_right = height(node.rightnode)
     return max(height_left, height_right)+1
-
 
 #lleno los Bf recursivamente 
 def BFrecursive(node):
@@ -67,8 +80,6 @@ def calculateBalance(AVLTree):
   else:
     return(BFrecursive(AVLTree.root))
 
-
-
 #imprime un arbol binario, hay que pasar la raiz
 def imprimir_arbol(node, prefix="", is_left=True):
     if node is None:
@@ -79,9 +90,33 @@ def imprimir_arbol(node, prefix="", is_left=True):
     if node.leftnode != None:
         imprimir_arbol(node.leftnode, prefix + ("    " if is_left else "│   "), True)
 
+#funcion que balancea un arbol recursivamente
+def recursiveBalance(node,Tree):
+  if node == None:
+    return
+  if node.bf == 2: #caso para cuando hay que girar a la derecha
+    if node.rightnode != None:
+      if node.rightnode.bf == -1:
+        rotateLeft(Tree,node.leftnode)
+    rotateRight(Tree,node)
+  if node.bf == -2: #caso para cuando hay que girar a la izqiuerda
+    if node.rightnode != None:
+      if node.rightnode.bf == 1:
+        print("entra a el if que tiene un hijo izquierdo")
+        rotateRight(Tree,node.rightnode)
+    rotateLeft(Tree,node)
+  recursiveBalance(node.leftnode,Tree)
+  recursiveBalance(node.rightnode,Tree)
 
-''' Descripción: Inserta un elemento con una clave determinada del TAD árbol binario.'''
-'''Salida: Si pudo insertar con éxito devuelve la key donde se inserta el elemento. En caso contrario devuelve None. '''
+
+#balancea un arbol binario de busqueda 
+def reBalance(AVLTree):
+  if AVLTree == None:
+    return None
+  else:
+    return(recursiveBalance(AVLTree.root,AVLTree))
+
+#inserta un nodo en un arbol binario hay que modificarlo para que sea avl 
 def insert(B, element, key):
   newNode = AVLNode()
   newNode.key = key
@@ -91,7 +126,8 @@ def insert(B, element, key):
   if (B.root == None):
     B.root = newNode
     return key
-  return insertR(newNode, B.root)
+  insertR(newNode, B.root)
+  return(reBalance(B))
 
 # Función recursiva de insert
 def insertR(newNode, currentNode):
@@ -114,3 +150,84 @@ def insertR(newNode, currentNode):
       if left != None:
         return left
 
+#funcion que elimina un nodo de un arbol binario    
+def deleteKey(B, key):
+  node = searchKeyR(B.root, key)
+  if node == None: return
+  else:
+    deleteCurrentCase(B, node)
+    calculateBalance(B)
+    return reBalance(B)
+
+# Devuelve el elemento con mayor key desde un nodo dado
+def bigger(node):
+  if node.rightnode != None:
+    current = bigger(node.rightnode)
+    if current != None:
+      return current
+  else:
+    return node
+
+#busca un nodo por su key
+def searchKey(B, key):
+  node = searchKeyR(B.root, key)
+# Función recursiva de searchKey
+def searchKeyR(node, key):
+  if node == None: return
+
+  if node.key == key:
+    return node
+
+  right = searchKeyR(node.rightnode, key)
+  if right != None:
+    return right
+
+  left = searchKeyR(node.leftnode, key)
+  if left != None:
+    return left
+#elimina segun el caso del nodo
+def deleteCurrentCase(B, node):
+  if node.rightnode == None:
+    if node.leftnode == None:
+
+      # Caso 1: El nodo a eliminar es una hoja
+      if node.parent.leftnode != None and node.parent.leftnode == node:
+        node.parent.leftnode = None
+        return node.key
+      elif node.parent.rightnode != None and node.parent.rightnode == node:
+        node.parent.rightnode = None
+        return node.key
+
+    # Caso 2: El nodo a eliminar tiene un hijo del lado izquierdo
+    if node.parent.leftnode != None and node.parent.leftnode == node:
+      node.parent.leftnode = node.leftnode
+      return node.key
+    elif node.parent.rightnode != None and node.parent.rightnode == node:
+      node.parent.rightnode = node.leftnode
+      return node.key
+
+  else:
+    # Caso 2: El nodo a eliminar tiene un hijo del lado derecho
+    if node.leftnode == None:
+      if node.parent.leftnode == node:
+        node.parent.leftnode = node.rightnode
+        return node.key
+      elif node.parent.rightnode == node:
+        node.parent.rightnode = node.rightnode
+        return node.key
+    else:
+      # Caso 3: El nodo a eliminar tiene dos hijos'
+
+      # o eliminar el mayor de sus menores
+      changeNode = bigger(node.leftnode)
+
+      node.value = changeNode.value
+      oldKey = node.key
+      node.key = changeNode.key
+
+      if changeNode.parent.leftnode == changeNode:
+        changeNode.parent.leftnode = None
+      elif changeNode.parent.rightnode == changeNode:
+        changeNode.parent.rightnode = None
+
+      return oldKey
--- practicas/tp-arboles-avl/main.py
+++ practicas/tp-arboles-avl/main.py
@@ -4,19 +4,34 @@ from avltree import *
 B = AVLTree()
 
 insert(B,"A",5)
-insert(B,"B",2)
-insert(B,"C",1)
-
-#rotateLeft(B,B.root)
-
-imprimir_arbol(B.root)
-a=0
-a = height(B.root)
-print("la altura de B es: ",a)
-
+insert(B,"B",3)
+insert(B,"C",6)
+insert(B,"D",7)
+insert(B,"E",8)
 calculateBalance(B)
+print("peimera impresion del arbol original")
+imprimir_arbol(B.root)
+reBalance(B)
 
-print("los bf del arbol son")
-print(B.root.bf)
-print(B.root.leftnode.bf)
-print(B.root.leftnode.leftnode.bf)
+# a=0
+# a = height(B.root)
+# print("la altura de B es: ",a)
+#rotateRight(B,B.root)
+# calculateBalance(B)
+#rotateLeft(B,B.root)
+print("ARBOL BALANCEADO")
+imprimir_arbol(B.root)
+# print("arbol rotado a la izquierda del segundo nodo")
+# rotateLeft(B,B.root.rightnode)
+# imprimir_arbol(B.root)
+# print("el padre del nodo rotado es: ", B.root.rightnode.parent.key)
+
+
+# rotateRight(B,B.root.rightnode)
+# print("arbol rotado a la derecha del segundo nodo")
+# imprimir_arbol(B.root)
+# #print(B.root.rightnode.rightnode.parent.key)
+# # print("los bf del arbol son")
+# # print(B.root.rightnode.bf)
+# # print(B.root.leftnode.bf)
+# print(B.root.rightnode.rightnode)
