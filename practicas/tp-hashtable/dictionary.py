99935149f63b72ff3435f56bb7aefab3f1da950e
JulianIgnFer24 <f.julian2617@gmail.com>
2024-05-01 11:44:17 -0300

    Ejercicio 2 de hashTable

---
+++ practicas/tp-hashtable/dictionary.py
@@ -0,0 +1,46 @@
+import math
+
+def functionHash(k):
+    hashnumber = 
+    return hashnumber
+
+m = 9
+dictionary =[None] * 9
+
+def insert(D, key, value):
+    hashnumber = functionHash(key)
+    if D[hashnumber] == None:
+        D[hashnumber] = [(key,value)]
+    else:
+        D[hashnumber].append(key,value)
+
+
+def search(D, key):
+    hashnumber = functionHash(key)
+    if D[hashnumber] == None:
+        return None
+    else:
+        for i in range(len(D[hashnumber])):
+            if D[hashnumber][i][0] == key:
+                return D[hashnumber][i][1]
+        return None
+
+def delete(D,key):
+    hashnumber = functionHash(key)
+    if D[hashnumber] == None:
+        return None
+    else:
+        for i in range(0,len(D[hashnumber])):
+            if D[hashnumber][i][0] == key:
+                D[hashnumber].pop(i)
+                return
+            
+
+insert(dictionary, 1, "one")
+insert(dictionary, 2, "two")
+insert(dictionary, 3, "three")
+insert(dictionary, 4, "four")
+
+print(dictionary)
+print(search(dictionary, 1))
+    
